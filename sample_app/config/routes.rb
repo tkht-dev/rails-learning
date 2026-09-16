c20512c3c601dc098a67720cbcc8a59a63d6b19c
tkht-dev <266846337+tkht-dev@users.noreply.github.com>
2026-09-16 22:12:26 +0900

    Add About page and static page tests

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'static_pages/home'
   get 'static_pages/help'
     root "application#hello"
+    get "static_pages/about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
